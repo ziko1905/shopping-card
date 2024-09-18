e751355ddd54456ce9707821c36bee692faa6540
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-18 02:09:40 +0200

    Setup react routing

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,10 @@
+function ErrorPage() {
+    return (
+        <>
+            <h1>Temporary error page</h1>
+            <span>But let that not kid you, it is real error!</span>
+        </>
+    )
+}
+
+export default ErrorPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './routes.jsx'
 import './index.css'
 import './default.css'
 
+const router = createBrowserRouter(routes)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
