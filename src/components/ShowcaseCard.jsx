7e1287b18a8327e8d847e4143e9b4bb91d7530c8
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-19 07:27:11 +0200

    Add and test showcase adding to cart

--- src/components/ShowcaseCard.jsx
+++ src/components/ShowcaseCard.jsx
@@ -2,13 +2,24 @@ import { useState } from "react"
 import Card from "./Card.jsx"
 
 function ShowcaseCard(props) {
-    const [amount, setAmount] = useState(0)
-    const addToCartBtn = { text: "Add To Cart" }
+    const [amount, setAmount] = useState(1)
+    const addToCartBtn = { text: "Add To Cart", onClick: () => handleOrderUpdate({ productId: props.productId, amount: amount}) }
 
     function handleAmountChange(newAmount) {
-        if (newAmount < 0 || isNaN(+newAmount)) return
+        if (newAmount < 1 || isNaN(+newAmount)) return
         else setAmount(Math.floor(newAmount))
     }    
+
+    function handleOrderUpdate(obj) {
+        const oldOrderElem = props.order.filter(order => order.productId === obj.productId)[0]
+        if (oldOrderElem) {
+            oldOrderElem.amount += obj.amount
+            props.orderCallback(props.order)
+        }
+        else {
+            props.orderCallback([...props.order, obj])
+        } 
+    }
 
     return (
         <Card {...props} amountCallback={handleAmountChange} amount={amount} addOnBtns={[addToCartBtn]}/>
--- src/components/ShowcaseCard.test.jsx
+++ src/components/ShowcaseCard.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
-import { screen, render, getByAltText } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import ShowcaseCard from "./ShowcaseCard.jsx"
 
 describe("Showcase card general", () => {
@@ -23,7 +23,7 @@ describe("Amount logic", () => {
 
         await user.click(screen.getByLabelText("Decrement amount by one"))
         rerender(<ShowcaseCard />)
-        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("0")
+        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("1")
     })
     it("Remove floats for amount", async () => {
         const user = userEvent.setup()
@@ -34,25 +34,65 @@ describe("Amount logic", () => {
         rerender(<ShowcaseCard />)
         await user.keyboard(".")
         rerender(<ShowcaseCard />)
-        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("5")
+        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("15")
     })
     it("Non numbers don't load", async () => {
         const user = userEvent.setup()
         const {rerender} = render(<ShowcaseCard />)
 
         await user.click(screen.getByLabelText("Input for changing amount with current amount"))
-        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("0")
+
+        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("1")
 
         await user.keyboard("N")
         rerender(<ShowcaseCard />)
-        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("0")
+
+        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("1")
 
         await user.keyboard("5")
         rerender(<ShowcaseCard />)
-        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("5")
+
+        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("15")
 
         await user.keyboard("a")
+
         rerender(<ShowcaseCard />)
-        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("5")
+        expect(screen.getByLabelText("Input for changing amount with current amount").value).toBe("15")
+    })
+})
+
+describe("Adding to order", () => {
+    it("Add first item", async () => {
+        const user = userEvent.setup()
+        const orderFn = vi.fn()
+        const order = []
+        render(<ShowcaseCard productId={1} order={order} orderCallback={orderFn}/>)
+
+        await user.click(screen.getByText("Add To Cart"))
+
+        expect(orderFn).toHaveBeenCalled()
+        expect(orderFn.mock.calls[0][0]).toStrictEqual([{productId: 1, amount: 1}])
+    })
+    it("Add new item to old list", async () => {
+        const user = userEvent.setup()
+        const orderFn = vi.fn()
+        const order = [{ productId: 2, amount: 2 }]
+        render(<ShowcaseCard productId={1} order={order} orderCallback={orderFn}/>)
+
+        await user.click(screen.getByText("Add To Cart"))
+
+        expect(orderFn).toHaveBeenCalled()
+        expect(orderFn.mock.calls[0][0]).toStrictEqual([{ productId: 2, amount: 2 }, {productId: 1, amount: 1, }])
+    })
+    it("Adding item that is already in order", async () => {
+        const user = userEvent.setup()
+        const orderFn = vi.fn()
+        const order = [{ productId: 2, amount: 2 }, { productId: 1, amount: 2 }]
+        render(<ShowcaseCard productId={2} order={order} orderCallback={orderFn}/>)
+
+        await user.click(screen.getByText("Add To Cart"))
+
+        expect(orderFn).toHaveBeenCalled()
+        expect(orderFn.mock.calls[0][0]).toStrictEqual([{ productId: 2, amount: 3 }, { productId: 1, amount: 2 }])
     })
 })
