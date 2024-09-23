8aee9113068d25b71def92a0d391cb800578ae72
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-23 13:25:01 +0200

    Test and fix order managment within App

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import Navbar from './components/Navbar'
 import { Outlet, Route, Routes } from 'react-router-dom'
 import Homepage from './components/pages/Homepage'
 import ShoppingCart from './components/pages/ShoppingCart'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [order, setOrder] = useState([])
+  const [productList, setProductList] = useState([])
+  
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/').then(response => response.json())
+    .then(response => setProductList(response))
+  }, [])
+  
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/*' element={<Homepage order={[]}/>} />
-        <Route path='shopping-cart' element={<ShoppingCart order={[]}/>} />
+        <Route path='/*' element={<Homepage orderCallback={setOrder} order={order} productList={productList} />} />
+        <Route path='shopping-cart' element={<ShoppingCart orderCallback={setOrder} order={order}/>} />
       </Routes>
     </>
   )
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,18 +1,98 @@
-import { describe, it, expect } from 'vitest';
-import { screen, render } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { screen, render, waitFor } from '@testing-library/react';
 import App from '../App';
 import { MemoryRouter } from 'react-router-dom';
+import { useEffect } from 'react';
+
+let mockProductListProp
+let mockOrderProp
+let mockProductListPropSC
+let mockOrderPropSC
+
+const mockProductList = await fetch('https://fakestoreapi.com/products/').then(response => response.json()).then()
+const mockOrder = ["pr1", "pr2"]
+
+vi.mock(import("../components/pages/Homepage.jsx"), () => ({
+  default: ({productList, orderCallback, order}) => {
+      
+      useEffect(() => {
+        mockProductListProp(productList)
+      }, [productList])
+
+      useEffect(() => {
+        mockOrderProp(order)
+      }, [order])
+      
+      if (JSON.stringify(order) !== JSON.stringify(mockOrder)) orderCallback(mockOrder)
+      return (
+        <>
+          <h1>Homepage</h1>
+        </>
+      )
+  }
+}))
+
+vi.mock(import("../components/pages/ShoppingCart.jsx"), () => ({
+  default: ({productList, orderCallback, order}) => {
+      
+      useEffect(() => {
+        mockProductListPropSC(productList)
+      }, [productList])
+
+      useEffect(() => {
+        mockOrderPropSC(order)
+      }, [order])
+      
+      if (JSON.stringify(order) !== JSON.stringify(mockOrder)) orderCallback(mockOrder)
+      return (
+        <>
+          <h1>Shopping Cart</h1>
+        </>
+      )
+  }
+}))
 
 describe('App general', () => {
   it('Renders at homepage', () => {
+    mockProductListProp = vi.fn()
+    mockOrderProp = vi.fn()
     render(<MemoryRouter initialEntries={["/"]}><App /></MemoryRouter>)
 
     expect(() => screen.getByRole('heading', {name: "Homepage"})).not.toThrow()
   })
   it('Renders at shopping cart', () => {
+    mockOrderPropSC = vi.fn()
+    mockProductListPropSC = vi.fn()
     render(<MemoryRouter initialEntries={["/shopping-cart"]}><App /></MemoryRouter>)
 
     expect(() => screen.getByRole('heading', {name: "Shopping Cart"})).not.toThrow()
-    screen.debug()
+  })
+  it('Passes props into routes', async () => {
+    mockProductListProp = vi.fn()
+    mockOrderProp = vi.fn()
+    render(<MemoryRouter initialEntries={["/"]}><App /></MemoryRouter>)
+
+    expect(mockProductListProp).toHaveBeenCalled()
+    expect(mockProductListProp.mock.calls[0][0]).toStrictEqual([])
+
+    await waitFor(() => expect(mockProductListProp).toHaveBeenCalledTimes(2))
+    expect(mockProductListProp.mock.calls[1][0]).toStrictEqual(mockProductList)
   })
 });
+
+describe('Order managing in within App', () => {
+  it("Set order changes order", async () => {
+    mockOrderProp = vi.fn()
+    render(<MemoryRouter initialEntries={["/"]}><App /></MemoryRouter>)
+
+    await waitFor(() => expect(mockOrderProp).toHaveBeenCalledTimes(2))
+    expect(mockOrderProp.mock.calls[1][0]).toStrictEqual(mockOrder)
+  })
+  it("Set order changes order(in shopping cart)", async () => {
+    mockOrderPropSC = vi.fn()
+    render(<MemoryRouter initialEntries={["/shopping-cart"]}><App /></MemoryRouter>)
+
+    await waitFor(() => expect(mockOrderPropSC).toHaveBeenCalledTimes(2))
+    expect(mockOrderPropSC.mock.calls[1][0]).toStrictEqual(mockOrder)
+  })
+})
